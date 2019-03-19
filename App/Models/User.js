7cea9c5316f7122afbda01748955fee04124544b
Unknown <benhamin.h9@gmail.com>
2019-03-19 13:30:40 +0100

    Fixed package bug

--- App/Models/User.js
+++ App/Models/User.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             validate: {
                 isEmail: true,
-                notNull: true
+                allowNull: true
             },
             unique: true
         },
